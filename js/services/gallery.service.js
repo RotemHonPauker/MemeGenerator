441d39796328dd56bd9127f78b835d372f54eca5
RotemHonPauker <84242449+RotemHonPauker@users.noreply.github.com>
2024-09-26 13:58:50 +0300

    add img

---
+++ js/services/gallery.service.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var gImgs = [
+    { id: '01', url: 'img/photo-gallery/2.jpg', keywords: ['happy', 'naive', 'movie'] },
+    { id: '02', url: 'img/photo-gallery/003.jpg', keywords: ['angry', 'speech', 'boomer'] },
+    { id: '03', url: 'img/photo-gallery/004.jpg', keywords: ['love', 'naive', 'dog'] },
+    { id: '04', url: 'img/photo-gallery/5.jpg', keywords: ['can-do', 'strong', 'baby'] },
+    { id: '05', url: 'img/photo-gallery/005.jpg', keywords: ['sleep', 'dog', 'baby'] },
+    { id: '06', url: 'img/photo-gallery/006.jpg', keywords: ['sleep', 'office', 'cat'] },
+    { id: '07', url: 'img/photo-gallery/8.jpg', keywords: ['happy', 'naive', 'movie'] },
+    { id: '08', url: 'img/photo-gallery/9.jpg', keywords: ['happy', 'sly', 'baby'] },
+
+]
+
+var gKeywordSearchCountMap = { 'strong': 1, 'naive': 5, 'baby': 2 }
---
+++ js/services/meme.service.js
@@ -0,0 +1,11 @@
+var gMeme = {
+    selectedImgId: '05',
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            color: 'red'
+        }
+    ]
+}
